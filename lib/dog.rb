a5ac60e6cd33e90c249b7a8c03643548486e6712
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 05:28:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,10 +6,24 @@ class Dog
     @@all << self
   end
   
-  def 
+  def self.all
+    
+  end
   
+  def self.clear_all
+    
+  end
   
+  def self.print_all
+    
+  end
   
+  def save
+    
+  end
   
-  
+  def name
+    
+  end
+
 end
